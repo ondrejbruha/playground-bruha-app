d5908c3e74d6e0c70b3dd1f6bfa3ce8ae0ae7592
Ondřej Brůha <bruhaon@gmail.com>
2023-01-30 20:19:57 +0100

    unit test

--- test/template_data.test.js
+++ test/template_data.test.js
@@ -3,17 +3,17 @@ const TemplateData = require("../server/abl/template_data/template_data");
 let templateData = new TemplateData();
 
 test("nois data generation", ()=>{
-    expect(templateData.generateRandomNois()).toBeInstanceOf(Array);
+    expect(templateData.generateRandomNois(0,1,100)).toBeInstanceOf(Array);
 });
 test("linear data", ()=>{
-    expect(templateData.generateLinearData()).toBeInstanceOf(Array);
+    expect(templateData.generateLinearData(0, 10,100)).toBeInstanceOf(Array);
 });
 test("polynomial data", ()=>{
-    expect(templateData.generatePolynomialData()).toBeInstanceOf(Array);
+    expect(templateData.generatePolynomialData(0,3,2,100)).toBeInstanceOf(Array);
 });
 test("sinus data", ()=>{
-    expect(templateData.generateSin()).toBeInstanceOf(Array);
+    expect(templateData.generateSin(1,20,100)).toBeInstanceOf(Array);
 });
 test("const data",()=>{
-    expect(templateData.generateConstant()).toBeInstanceOf(Array);
+    expect(templateData.generateConstant(6,100)).toBeInstanceOf(Array);
 });
