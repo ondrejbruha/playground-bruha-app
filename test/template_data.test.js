d59ed5d5687f1773446074ac1d7000f18c7da282
Ondrej Bruha <ondrej.bruha@outlook.com>
2023-02-06 19:53:24 +0100

    testy

--- test/template_data.test.js
+++ test/template_data.test.js
@@ -4,16 +4,20 @@ let templateData = new TemplateData();
 
 test("nois data generation", ()=>{
     expect(templateData.generateRandomNois(0,1,100)).toBeInstanceOf(Array);
+    expect(templateData.generateRandomNois(0,1,100)[2]).toBeGreaterThanOrEqual(0);
 });
 test("linear data", ()=>{
     expect(templateData.generateLinearData(0, 10,100)).toBeInstanceOf(Array);
+    expect(templateData.generateLinearData(0, 10,100)[2]).toBeGreaterThanOrEqual(0);
 });
 test("polynomial data", ()=>{
     expect(templateData.generatePolynomialData(0,3,2,100)).toBeInstanceOf(Array);
 });
 test("sinus data", ()=>{
-    expect(templateData.generateSin(1,20,100)).toBeInstanceOf(Array);
+    expect(templateData.generateSin(1,20,100)).toBeInstanceOf(Array)
+    expect(templateData.generateSin(1,20,100)[2]).toBeGreaterThanOrEqual(-1);
 });
 test("const data",()=>{
     expect(templateData.generateConstant(6,100)).toBeInstanceOf(Array);
+    expect(templateData.generateConstant(6,100)[2]).toBe(6);
 });
